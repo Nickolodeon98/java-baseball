85f3d7d4bc195150fb58ae877308b47df017de7e
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-07 23:56:06 +0900

    Add: 첫 번째 기능, 무작위의 세 개의 숫자를 리스트에 저장하여 가지고 있는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public List<Integer> createRndNumbers() {
+        List<Integer> computerSide = new ArrayList<>();
+
+        while(computerSide.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerSide.contains(randomNumber)) computerSide.add(randomNumber);
+        }
+
+        return computerSide;
     }
 }
